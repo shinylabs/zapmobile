85b27f08102c6ef4d29f301142192300ff5cb927
jw84 <jerrywang@me.com>
2015-11-30 17:51:32 -0800

    got tabs now

--- SecureView.js
+++ SecureView.js
@@ -1,53 +1,55 @@
-"use strict";
- 
-var React = require("react-native");
-var ScrollableTabView = require('react-native-scrollable-tab-view');
+'use strict';
 
+var React = require('react-native');
+var Tabs = require('react-native-tabs');
 var {
     Component,
     StyleSheet,
     Text,
     View,
 } = React;
- 
 
 class SecureView extends Component {
- 
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: this.props.username,
-            password: this.props.password
-        };
-    }
- 
-    render() {
-        return (
-                <ScrollableTabView>
-                    <ReactPage tabLabel="React" />
-                    <FlowPage tabLabel="Flow" />
-                    <JestPage tabLabel="Jest" />
-                </ScrollableTabView>
-        );
-    }
- 
-};
- 
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+  render() {
+    var self = this;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.instructions}>
+          {this.state.page} goes here
+        </Text>
+        <Tabs selected="Home" style={{backgroundColor:'white'}}
+              onSelect={function(el){self.setState({page: el.props.name});return {style:{color:'red'}}}}>
+            <Text name="Activity">Activity</Text>
+            <Text name="Communities">Communities</Text>
+            <Text name="Profile">Profile</Text>
+            <Text name="New Post">New Post</Text>
+        </Tabs>
+      </View>
+    );
+  }
+}
+
 var styles = StyleSheet.create({
-    container: {
-        padding: 30,
-        marginTop: 65,
-        alignItems: "center"
-    },
-    heading: {
-        marginBottom: 20,
-        fontSize: 18,
-        textAlign: "center",
-        color: "#656565"
-    },
-    subheading: {
-        color: "#cccccc"
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
- 
+
 module.exports = SecureView;
